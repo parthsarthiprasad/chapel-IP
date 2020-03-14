e225221cb4ed9e02c2bdb9f787078274180d1ca8
parthsarthiprasad <mepsp2000@gmail.com>
2020-03-14 14:47:20 +0530

    jpeg writefile support added

--- jpeg/img_jpeg_v1.c
+++ jpeg/img_jpeg_v1.c
@@ -383,81 +383,109 @@ int JPEG_read (const char * fname , rgbimage **img)
     returns:   0 if successful
                < 0 on failure (value depends on error)
 ***/
-int JPEG_write(const char *fname, rgbimage *img) {
-  FILE *fout;                           /* file handle to write to */
-  png_structp ptr;                      /* internal reference to PNG data */
-  png_infop info;                       /* picture information */
-  png_byte *row;                        /* copy of image row to write */
-  char *errmsg;                         /* error message */
-  int x, y, xy;                         /* pixel coordinates/index */
-  int i;
-  int retval;
+int JPEG_write (const char * fname, int quality = 75,  rgbimage *img )
+{
+  /* This struct contains the JPEG compression parameters and pointers to
+   * working space (which is allocated as needed by the JPEG library).
+   * It is possible to have several such structures, representing multiple
+   * compression/decompression processes, in existence at once.  We refer
+   * to any one struct (and its associated working data) as a "JPEG object".
+   */
+  struct jpeg_compress_struct cinfo;
+  /* This struct represents a JPEG error handler.  It is declared separately
+   * because applications often want to supply a specialized error handler
+   * (see the second half of this file for an example).  But here we just
+   * take the easy way out and use the standard error handler, which will
+   * print a message on stderr and call exit() if compression fails.
+   * Note that this struct must live as long as the main JPEG parameter
+   * struct, to avoid dangling-pointer problems.
+   */
+  struct jpeg_error_mgr jerr;
+  /* More stuff */
+  FILE * fout;		/* target file */
+  JSAMPROW row_pointer[1];	/* pointer to JSAMPLE row[s] */
+  int row_stride;		/* physical row width in image buffer */
+	int retval=-1;
 
-  fout = NULL;
-  row = NULL;
+  /* Step 1: allocate and initialize JPEG compression object */
 
-  if (NULL == fname) {
-    fout = stdout;
-  } else {
-    /* For Windows, "wb". */
-    fout = fopen(fname, "w");
-    if (NULL == fout) {
-      errmsg = strerror(errno);
-      printf("can't open file %s to write: %s\n", fname, errmsg);
-      return -1;
-    }
+  /* We have to set up the error handler first, in case the initialization
+   * step fails.  (Unlikely, but it could happen if you are out of memory.)
+   * This routine fills in the contents of struct jerr, and returns jerr's
+   * address which we place into the link field in cinfo.
+   */
+  cinfo.err = jpeg_std_error(&jerr);
+  /* Now we can initialize the JPEG compression object. */
+  jpeg_create_compress(&cinfo);
+
+  /* Step 2: specify data destination (eg, a file) */
+  /* Note: steps 2 and 3 can be done in either order. */
+
+  /* Here we use the library-supplied code to send compressed data to a
+   * stdio stream.  You can also write your own code to do something else.
+   * VERY IMPORTANT: use "b" option to fopen() if you are on a machine that
+   * requires it in order to write binary files.
+   */
+  if ((fout = fopen(fname, "wb")) == NULL) {
+    printf(stderr, "can't open %s\n", fame);
+    return -1;
+		goto cleanup;
   }
+  jpeg_stdio_dest(&cinfo, fout);
 
-  if (NULL == (row = (png_byte *) calloc(3 * img->ncol, sizeof(png_byte)))) {
-    printf("can't allocate local row storage\n");
-    retval = -1;
-    goto cleanup;
+  /* Step 3: set parameters for compression */
+
+  /* First we supply a description of the input image.
+   * Four fields of the cinfo struct must be filled in:
+   */
+  cinfo.image_width = rgbimage.ncol; 	/* image width and height, in pixels */
+  cinfo.image_height = rgbimage.nrow;
+  cinfo.input_components = 3;		/* # of color components per pixel */
+  cinfo.in_color_space = JCS_RGB; 	/* colorspace of input image */
+  /* Now use the library's routine to set default compression parameters.
+   * (You must set at least cinfo.in_color_space before calling this,
+   * since the defaults depend on the source color space.)
+   */
+  jpeg_set_defaults(&cinfo);
+  /* Now you can set any non-default parameters you wish to.
+   * Here we just illustrate the use of quality (quantization table) scaling:
+   */
+  jpeg_set_quality(&cinfo, quality, TRUE /* limit to baseline-JPEG values */);
+
+  /* Step 4: Start compressor */
+
+  /* TRUE ensures that we will write a complete interchange-JPEG file.
+   * Pass TRUE unless you are very sure of what you're doing.
+   */
+  jpeg_start_compress(&cinfo, TRUE);
+
+  /* Step 5: while (scan lines remain to be written) */
+  /*           jpeg_write_scanlines(...); */
+
+  /* Here we use the library's state variable cinfo.next_scanline as the
+   * loop counter, so that we don't have to keep track ourselves.
+   * To keep things simple, we pass one scanline per call; you can pass
+   * more if you wish, though.
+   */
+  row_stride = cinfo.image_width * 3;	/* JSAMPLEs per row in image_buffer */
+
+  while (cinfo.next_scanline < cinfo.image_height) {
+    /* jpeg_write_scanlines expects an array of pointers to scanlines.
+     * Here the array is only one element long, but you could pass
+     * more than one scanline at a time if that's more convenient.
+     */
+    //row_pointer[0] = & image_buffer[cinfo.next_scanline * row_stride];
+    row_pointer[0] = img->r+cinfo.next_scanline*row_stride;
+		(void) jpeg_write_scanlines(&cinfo, row_pointer, 1);
   }
+  
+  /* Step 6: Finish compression */
 
-  ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-  if (NULL == ptr) {
-    printf("could not prepare PNG for write\n");
-    retval = -1;
-    goto cleanup;
-  }
+  cleanup:
 
-  info = png_create_info_struct(ptr);
-  if (NULL == info) {
-    printf("could not prepare PNG info\n");
-    retval = -1;
-    goto cleanup;
-  }
-    
-  if ((png_jmpbuf(ptr))) {
-    retval = -1;
-    goto cleanup;
-  }
-
-  /* Prepare to write. */
-  png_init_io(ptr, fout);
-  png_set_IHDR(ptr, info, img->ncol, img->nrow, 8, PNG_COLOR_TYPE_RGB,
-               PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, 
-               PNG_FILTER_TYPE_DEFAULT);
-  png_write_info(ptr, info);
-
-  for (y=0, xy=0; y<img->nrow; y++) {
-    for (x=0, i=0; x<img->ncol; x++, xy++, i+=3) {
-      row[i] = img->r[xy];
-      row[i+1] = img->g[xy];
-      row[i+2] = img->b[xy];
-    }
-    png_write_row(ptr, row);
-  }
-
-  png_write_end(ptr, info);
-
-  retval = 0;
-
- cleanup:
-  if (row) {
-    free(row);
-  }
-
+	jpeg_finish_compress(&cinfo);
+	retval=0;
+  /* After finish_compress, we can close the output file. */
   if (NULL != fout) {
     retval = fclose(fout);
     if (0 != retval) {
@@ -466,19 +494,16 @@ int JPEG_write(const char *fname, rgbimage *img) {
       retval = -1;
     }
   }
+	
 
-  if (NULL != ptr) {
-    if (NULL != info) {
-      png_destroy_write_struct(&ptr, &info);
-    } else {
-      png_destroy_write_struct(&ptr, NULL);
-    }
-  }
+  /* Step 7: release JPEG compression object */
 
+  /* This is an important step since it will release a good deal of memory. */
+  jpeg_destroy_compress(&cinfo);
 
-  return retval;
+  /* And we're done! */
+	return retval;
 }
-
 
 
 /**** rgbimage Support ****/
